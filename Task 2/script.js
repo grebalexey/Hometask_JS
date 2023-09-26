d9314ccf887083ce159e02749b76f6eaf4430e92
grebalexey <136621926+grebalexey@users.noreply.github.com>
2023-09-26 22:42:14 +0300

    начал дз

---
+++ Task 2/script.js
@@ -0,0 +1,44 @@
+// 1. Подсчитать сумму всех чисел в заданном пользователем
+// диапазоне.
+
+// let number1 = +prompt("Введите первое число диапазона", "");
+// let number2 = +prompt("Введите последнее число диапазона", "");
+// let sum = 0;
+
+// while (number1 <= number2){
+//     sum = sum + number1;
+//     number1++;
+// }
+
+// alert (`Сумма всех чисел ${sum}`);
+
+
+// 2. Запросить 2 числа и найти только наибольший общий
+// делитель.
+
+// let number1 = +prompt("Введите первое число", "");
+// let number2 = +prompt("Введите второе число", "");
+
+// while (number1 != 0 && number2 != 0){
+//     if (number1 > number2){
+//         number1 = number1 % number2;
+//     } else if (number1 < number2){
+//         number2 = number2 % number1;
+//     }
+    
+// }
+
+// alert (`Наибольший общий делитель ${number1 + number2}`);
+
+
+
+// 3. Запросить у пользователя число и вывести все делители
+// этого числа.
+
+let number = +prompt("Введите число", "");
+
+for (let index = 1; index <= number; index++) {
+    
+    let divider
+    
+}
